d05884328f3b6276b87e5c65765b73d72778ba1c
Pedro Luiz <pedroluizlsg@gmail.com>
2022-10-19 17:12:21 -0300

    Req11 - fix - mostra apenas os dados da coluna notes

--- desafio11.sql
+++ desafio11.sql
@@ -1,4 +1,4 @@
-SELECT * FROM purchase_orders
+SELECT notes FROM purchase_orders
 WHERE notes IS NOT NULL
 AND notes BETWEEN 'Purchase generated based on Order #30' 
 AND 'Purchase generated based on Order #39';
